27e3af0917e8eb0466c0deda90da5523680f0fe7
BajiLinux <mr.baji.smart@gmail.com>
2026-06-27 23:17:30 +0700

    d-4->d-5->Getter Method

---
+++ 1-dapur-bintang-lima/cabang/restaurant.go
@@ -0,0 +1,26 @@
+package cabang
+
+import "fmt"
+
+type Restaurant struct {
+	Name string;
+	totalChef int;
+	Is24Hour bool;
+}
+
+// ? r itu adalah alias yang nantinya Restaurant itu jadi singkat yaitu r, dan ini tanpa * yang artinya
+func (r Restaurant) BukaToko() {
+	fmt.Println("Perhatian!, Restaurant: ", r.Name, " sudah dibuka")
+}
+
+func (r Restaurant) BuatPesanan(namaPesanan string) {
+	fmt.Println("Ada pesanan di Restaurant ", r.Name, " pesanan: ", namaPesanan)
+}
+
+func (r *Restaurant) TambahKoki(tambahan int) {
+	r.totalChef = r.totalChef + tambahan
+}
+
+func (r Restaurant) TambahKokii(tambahan int) {
+	r.totalChef = r.totalChef + tambahan
+}
--- 1-dapur-bintang-lima/main.go
+++ 1-dapur-bintang-lima/main.go
@@ -1,41 +1,18 @@
 package main
 
-import "fmt"
-
-// ? Cara membuat struct atau blueprint gampangnya
-type Restaurant struct {
-	Name string;
-	TotalChef int;
-	Is24Hour bool;
-}
-
-// ? r itu adalah alias yang nantinya Restaurant itu jadi singkat yaitu r, dan ini tanpa * yang artinya
-func (r Restaurant) BukaToko() {
-	fmt.Println("Perhatian!, Restaurant: ", r.Name, " sudah dibuka")
-}
-
-func (r Restaurant) BuatPesanan(namaPesanan string) {
-	fmt.Println("Ada pesanan di Restaurant ", r.Name, " pesanan: ", namaPesanan)
-}
-
-func (r *Restaurant) TambahKoki(tambahan int) {
-	r.TotalChef = r.TotalChef + tambahan
-}
-
-func (r Restaurant) TambahKokii(tambahan int) {
-	r.TotalChef = r.TotalChef + tambahan
-}
+import (
+	"dapur-bintang-lima/cabang"
+	"fmt"
+)
 
 func main () {
-	center := Restaurant{
+	center := cabang.Restaurant{
 		Name: "Baji Restaurant Pusat",
-		TotalChef: 100,
 		Is24Hour: true,
 	}
 
-	cirebonCity := Restaurant{
+	cirebonCity := cabang.Restaurant{
 		Name: "Baji Restaurant Cirebon City",
-		TotalChef: 40,
 		Is24Hour: true,
 	}
 	
@@ -43,20 +20,14 @@ func main () {
 	center.BukaToko()
 	center.BuatPesanan("Nasi Goreng")
 	fmt.Println(center.Name)
-	fmt.Println(center.TotalChef)
 	fmt.Println(center.Is24Hour)
-	center.TambahKoki(20)
-	// center.TambahKokii(20)
-	fmt.Println(center.TotalChef)
+	center.TambahKoki(100)
 
 	fmt.Println("")
 
 	cirebonCity.BukaToko()
 	cirebonCity.BuatPesanan("Nasi Lengko")
 	fmt.Println(cirebonCity.Name)
-	fmt.Println(cirebonCity.TotalChef)
 	fmt.Println(cirebonCity.Is24Hour)
-	cirebonCity.TambahKoki(20)
-	// cirebonCity.TambahKokii(20)
-	fmt.Println(cirebonCity.TotalChef)
+	cirebonCity.TambahKoki(100)
 }
